ed7f4749b19555cf54f54d0af6d425ea0bea8b21
uppturusubbaiah123-glitch <uppturusubbaiah123@gmail.com>
2026-02-02 11:11:43 +0530

    Time: 1311 ms (5.48%), Space: 53.1 MB (9.28%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        //uppturusubbaiah123
+        if(headA==null || headB==null)
+        {
+              return null;
+        }
+        ListNode a=headA;
+        ListNode b=headB;
+        while(a!=b)
+        {
+            a=(a==null) ? headA : a.next;
+            b=(b==null) ? headB : b.next;
+        }  
+        return a;  
+}
+}
